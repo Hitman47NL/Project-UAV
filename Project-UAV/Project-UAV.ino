570c0ee5979317f7388dd0c2b4e086b93f9d6f29
Hitman47NL <Iwanvanbezouw@gmail.com>
2024-02-12 13:54:13 +0100

    Add project files.

---
+++ Project-UAV/Project-UAV.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Project_UAV.ino
+ Created:	2/12/2024 1:53:25 PM
+ Author:	Iwanv
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

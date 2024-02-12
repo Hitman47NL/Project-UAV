4a523acdbbfda6f029fa4c0b5b0255d44fa1c340
Hitman47NL <Iwanvanbezouw@gmail.com>
2024-02-12 14:12:07 +0100

    change

---
+++ Regelaar Jari/Regelaar Jari.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Regelaar_Jari.ino
+ Created:	2/12/2024 2:09:34 PM
+ Author:	Iwanv
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Regelaar Jelle/Regelaar Jelle.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Regelaar_Jelle.ino
+ Created:	2/12/2024 2:10:20 PM
+ Author:	Iwanv
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}

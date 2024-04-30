d11468e3687e3654a90fdbbd55a9f4440052ce8b
Hitman47NL <Iwanvanbezouw@gmail.com>
2024-04-30 11:14:30 +0200

    testing de coordinates

--- Communicatie_arduino_naar_rpi/Communicatie_arduino_naar_rpi.ino
+++ Communicatie_arduino_naar_rpi/Communicatie_arduino_naar_rpi.ino
@@ -1,18 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(19, 18); // RX, TX
+float x, y;
+
 void setup() {
-  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(2, INPUT_PULLUP); // Button connected to pin 2
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String incomingData = Serial.readStringUntil('\n');
-    int x_coord, y_coord;
-    if (incomingData.startsWith("X") && incomingData.endsWith("Y")) {
-      x_coord = incomingData.substring(1, incomingData.indexOf("Y")).toInt();
-      y_coord = incomingData.substring(incomingData.indexOf("Y") + 1).toInt();
-      Serial.print("Received coordinates: X=");
-      Serial.print(x_coord);
-      Serial.print(", Y=");
-      Serial.println(y_coord);
+  if (digitalRead(2) == LOW) {
+    mySerial.println("READY");
+    while (mySerial.available() > 0) {
+      String data = mySerial.readStringUntil('\n');
+      if (data.length() > 0) {
+        int commaIndex = data.indexOf(',');
+        x = data.substring(0, commaIndex).toFloat();
+        y = data.substring(commaIndex + 1).toFloat();
+      }
     }
   }
 }

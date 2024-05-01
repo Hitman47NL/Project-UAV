d10145b526fc94d5e3c77b256b04cb30209a0472
Hitman47NL <Iwanvanbezouw@gmail.com>
2024-05-01 19:31:46 +0200

    com arduino to rpi
    alleen het returnen van de nieuwe coordinaten werkt nog niet

--- Communicatie_arduino_naar_rpi/Communicatie_arduino_naar_rpi.ino
+++ Communicatie_arduino_naar_rpi/Communicatie_arduino_naar_rpi.ino
@@ -1,10 +1,16 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(19, 18); // RX, TX
+// RX and TX pins
+const int RXPin = 19;
+const int TXPin = 18;
+SoftwareSerial mySerial(RXPin, TXPin);
 
 const int buttonPin = 2;
 int buttonState = 0;
-bool isWaitingForResponse = false;
+int lastButtonState = 0;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
   pinMode(buttonPin, INPUT);
@@ -13,20 +19,55 @@ void setup() {
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  
-  if (buttonState == LOW && !isWaitingForResponse) {
-    mySerial.println("0.0,0.0");
-    isWaitingForResponse = true; // Avoid sending more data until we get a response
-    Serial.println("Sent: 0.0,0.0");
-    delay(1000);
+  int reading = digitalRead(buttonPin);
+
+  // If the button state has changed,
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
   }
 
-  if (isWaitingForResponse && mySerial.available() > 0) {
-    String modifiedCoordinates = mySerial.readStringUntil('\n');
-    if (modifiedCoordinates.length() > 0) {
-      Serial.println("Received: " + modifiedCoordinates);
-      isWaitingForResponse = false;
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // If the button state has been changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // Only send the coordinates if the new button state is HIGH
+      if (buttonState == LOW) {
+        mySerial.println("0,0");  // Sending coordinates to Raspberry Pi
+        Serial.println("Sent coordinates: X=0, Y=0");
+      }
+    }
+  }
+
+  lastButtonState = reading;
+
+  // Check if Raspberry Pi has sent back modified coordinates
+  if (mySerial.available()) {
+    String coordinates = mySerial.readStringUntil('\n');
+    coordinates.trim();  // Remove any whitespace or newline characters
+
+    if (coordinates.length() > 0) {
+      Serial.print("Received modified coordinates: ");
+      Serial.println(coordinates);
+
+      // Optional: Parsing the coordinates
+      int commaIndex = coordinates.indexOf(',');
+      if (commaIndex != -1) {
+        String xStr = coordinates.substring(0, commaIndex);
+        String yStr = coordinates.substring(commaIndex + 1);
+        
+        int x = xStr.toInt();
+        int y = yStr.toInt();
+        
+        Serial.print("X: ");
+        Serial.print(x);
+        Serial.print(", Y: ");
+        Serial.println(y);
+      } else {
+        Serial.println("Error: Received string is not in expected format.");
+      }
+    } else {
+      Serial.println("Error: No data received.");
     }
   }
 }

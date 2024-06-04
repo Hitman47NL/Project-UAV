7b4bcdbd5d9c8038df3cb2d105c67e7bd61fb4a0
BramHeerschap <bramheerschap13@gmail.com>
2024-06-04 12:16:07 +0200

    kleine aanpassingen

--- motoren/main_code.py
+++ motoren/main_code.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 #Een negatief senario is als de motor in de positieve richting van de Y-as beweegt waardoor de weegschaal een negatieve waarde uitleest 
 
-#import data.motor1_motor2_motorklein #positieve en negatieve scenario's van motor 1, motor 2 en de kleine motor
-#import data.propellors #staaf diagram van de maximale stuwkracht van de verschillende propellors
-#import data.motor1_vs_motor2_neg #vergelijking van de negatieve scenario's van motor 1 en motor 2
-#import data.motor1_vs_motor2_pos #vergelijking van de positieve scenario's van motor 1 en motor 2
+#import data.motor1_motor2_motorklein #positieve en negatieve scenario's van motor 1, motor 2 en de kleine motor voor de standaard propellor
+import data.propellors #staaf diagram van de maximale stuwkracht van de verschillende propellors
+#import data.motor1_vs_motor2_neg #vergelijking van de negatieve scenario's van motor 1 en motor 2 met standaard propellor
+#import data.motor1_vs_motor2_pos #vergelijking van de positieve scenario's van motor 1 en motor 2 
 import data.motor1_motor2_3blad_motorklein
 import data.motor1_vs_motor2_3blade_neg
 import data.motor1_vs_motor2_3blade_pos

11d1e947435bdf340a0406a0853327c2a8b557c8
TeunvdWater <106593759+Protewa@users.noreply.github.com>
2024-06-17 11:50:21 +0200

    data sdkaart

--- HV4/HovercraftV4/HovercraftV4.ino
+++ HV4/HovercraftV4/HovercraftV4.ino
@@ -10,6 +10,8 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include "ACS712.h"
+#include <SPI.h>
+#include <SD.h>
 
 //Define pinnen voor Arduino
 //PWM
@@ -20,6 +22,7 @@
 #define motorLinks 10     //  Motor B Input B
 #define motorRechtsPWM 9  //  Motor A Input A
 #define motorRechts 8     //  Motor A Input B
+#define chipSelect 53  // CS pin voor de SD-kaartmodule - pas deze aan naar de CS pin van jouw bordje!
 
 //Digitale pinnen
 #define BLOWRELAY 12
@@ -103,6 +106,89 @@ void Emergency() {
     Serial.println("PANIEK!!!");
   }
 }
+struct SensorData {
+  long time;
+  float value1;
+  float value2;
+  float value3;
+};
+
+// Definieer maximale grootte van buffer
+#define maxBufferSize 4096
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialisatie van de SD-kaart
+  if (!SD.begin(chipSelect)) {
+    Serial.println("\nInitialisatie van de SD-kaart mislukt!");
+    return;
+  }
+  Serial.println("\nInitialisatie van de SD-kaart voltooid.");
+}
+
+void writeDataToFile(byte *buffer, unsigned bufferSize) {
+  unsigned fileCounter = 0;
+  String filename;
+  do {
+    filename = "data" + String(fileCounter) + ".bin";
+    fileCounter++;
+  } while (SD.exists(filename)); // Controleer of het bestand al bestaat
+  File dataFile = SD.open(filename, FILE_WRITE);
+  
+  if (dataFile) {
+    Serial.println("Schrijven naar " + filename + "...");
+    // Schrijven van de buffer naar het bestand
+    dataFile.write(buffer, bufferSize);
+    
+    // Sluiten van het bestand
+    dataFile.close();
+    Serial.println("Schrijven naar " + filename + " voltooid");
+  } else {
+    Serial.println("Fout bij het openen van het bestand");
+  }
+}
+
+void data() {
+  static const unsigned numElements = maxBufferSize / sizeof(SensorData); // Bereken hoeveel elementen (structs) er weggeschreven kunnen worden in de buffer.
+  static SensorData buffer[numElements];
+  static boolean stopWriting = false;
+  static const long samplingTimeDataLogging = 100;  // ms, bepaalt frequentie waarmee sensordata wordt weggeschreven.
+  static const long timeStopWriting = 60000;  // ms, bepaalt frequentie waarmee sensordata wordt weggeschreven.
+  static long t_lastDataLog = 0, t_new;  // ms, ms;
+  static int currentIndex = 0;
+
+  t_new = millis();    // Lees de tijd
+  if (t_new - t_lastDataLog > samplingTimeDataLogging) {
+    t_lastDataLog = t_new;
+
+    if (!stopWriting){
+      buffer[currentIndex].time = t_new;
+
+      // Vul de struct met je meetwaardes. 
+      buffer[currentIndex].value1 = currentIndex * 1.5;  
+      buffer[currentIndex].value2 = currentIndex * 2.5;
+      buffer[currentIndex].value3 = currentIndex * 3.5;
+      
+      currentIndex++;
+
+      // Schrijf volle buffer weg naar een bestand
+      if (currentIndex == numElements)
+      {
+        writeDataToFile((byte*)buffer, numElements * sizeof(SensorData));
+        currentIndex = 0;
+      }
+
+      // Stop na een bepaalde tijd met het wegschrijven van bestanden
+      if(t_new > timeStopWriting)
+      {
+        Serial.println("Gestopt met meetdata wegschrijven.");
+        stopWriting = true;
+      }
+    }
+  }
+}
+
 
 void setup() {
   //Communicatie
